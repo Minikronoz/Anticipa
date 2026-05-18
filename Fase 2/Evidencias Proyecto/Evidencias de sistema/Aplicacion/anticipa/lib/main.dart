afa18dd6df70db5c10cabb217f380d3f80e2ed08
LaGu5 <agu.jara@duocuc.cl>
2026-05-17 23:58:21 -0400

    cambios en main.dart

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicacion/anticipa/lib/main.dart
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicacion/anticipa/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'screens/panel_profesor.dart';
 
 void main() {
   runApp(const AnticipaApp());
