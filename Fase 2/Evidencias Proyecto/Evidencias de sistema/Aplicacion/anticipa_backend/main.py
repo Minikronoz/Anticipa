424d10af850c69ffd165df28873a3bd317913a54
Minikronoz <107058226+Minikronoz@users.noreply.github.com>
2026-07-05 14:32:10 -0400

    fix errores

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicacion/anticipa_backend/main.py
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema/Aplicacion/anticipa_backend/main.py
@@ -472,7 +472,7 @@ def listar_estudiantes(db: Session = Depends(get_db)):
         selectinload(Estudiante.curso_r)
     ).all()
 
-        conteos = db.query(
+    conteos = db.query(
         EncuestaDiaria.estudiante_id_estudiante,
         func.coalesce(func.sum(EncuestaDiaria.cantidad), 0).label("total")
     ).filter(
